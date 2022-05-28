fb8aa2ae5895caa5d5f6796b0e7ac02dffd67fdc
Clayton <claytonwambu@gmail.com>
2022-05-28 23:14:18 +0100

    Correctly deserialises & extracts definitions from response

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
+use serde_json::Value;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
         .await?
         .text()
         .await?;
-    println!("{}", response);
+    let data = serde_json::from_str::<Value>(&response)?;
+
+    let meanings = &data[0]["meanings"];
+    let meanings = meanings.as_array().unwrap();
+
+    let mut defs = Vec::new();
+    for meaning in meanings {
+        defs.push(meaning["definitions"][0]["definition"].to_string())
+    }
+
+    for def in defs.iter_mut() {
+        *def = def.replace('\\', "");
+    }
+
+    for def in defs {
+        println!("{}", def)
+    }
 
     Ok(())
 }
