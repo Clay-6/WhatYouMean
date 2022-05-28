24cd75a167926726cae087d5e88959234e4d12ae
Clayton <claytonwambu@gmail.com>
2022-05-28 22:47:21 +0100

    Able to get data from `Free Dictionary API`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+        .await?
+        .text()
+        .await?;
+    println!("{}", response);
+
+    Ok(())
 }
