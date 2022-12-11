be9d7c1c6829976753d10433001a80e961d7179f
Clayton <claytonwambu@gmail.com>
2022-12-11 12:34:53 +0000

    Improve help text

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct Args {
+    /// The word to see definitions for
     pub word: Option<String>,
     /// Override API key in the `WORDNIK_API_KEY`
     /// environment variable
@@ -11,6 +12,8 @@ pub struct Args {
     /// Search for a random word
     #[clap(short, long)]
     pub random: bool,
+    /// The maximum amount of definitions to
+    /// display
     #[clap(long, default_value_t = 10)]
     pub max: usize,
 }
